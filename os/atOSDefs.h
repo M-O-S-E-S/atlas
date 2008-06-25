0dd7433a3176809b6cf39f63e35bd58b94bdb531
Glenn Martin <martin@ist.ucf.edu>
2008-06-25 19:00:24 +0000

    Added symbol handling and added a new "os" directory to hold OS-specific implementations (we are re-working the method used).

---
+++ os/atOSDefs.h
@@ -0,0 +1,12 @@
+
+#ifndef AT_OS_DEFS_H
+#define AT_OS_DEFS_H
+
+
+// Include other files to give OS-independent interfaces
+#include "atSymbols.h"
+#include "atSleep.h"
+
+
+#endif
+
---
+++ os/atSymbols.h
@@ -0,0 +1,37 @@
+
+#ifndef AT_SYMBOLS_H
+#define AT_SYMBOLS_H
+
+
+// Symbol-specific linkage directives.  Windows requires explicit export
+// and import instructions for its shared libraries (DLL's) and GCC can
+// provide such capabilities as well (although it's not required).
+// The following sections define tokens that specify the linkage and
+// are used in the Makefile (or Project Settings for Windows).  
+//
+// Constants for the appropriate library (or even sub-library) are
+// defined to one of these constants in the Makefile.  For example,
+//
+//   g++ -o test test.c++ -DATLAS_SYM=IMPORT
+//
+// and a class might be defined such as:
+// 
+// class ATLAS_SYM Test
+// {
+// };
+//
+// Typical use would export symbols when making a library that
+// will be used by others, and import symbols when making an
+// application that uses the library.
+
+#ifdef WIN32
+   #define IMPORT __declspec(dllimport)
+   #define EXPORT __declspec(dllexport)
+#else
+   #define IMPORT __attribute__((visibility("default")))
+   #define EXPORT __attribute__((visibility("default")))
+#endif
+
+
+#endif
+
