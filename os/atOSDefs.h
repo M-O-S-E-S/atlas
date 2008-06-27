dd0faa831bd37c2527d5ebd195696a1a3bc55748
Glenn Martin <martin@ist.ucf.edu>
2008-06-27 02:26:00 +0000

    Added new semaphore and shared memory support.

--- os/atOSDefs.h
+++ os/atOSDefs.h
@@ -11,6 +11,8 @@
 // Include other files to give OS-independent interfaces
 #include "atErrno.h"
 #include "atNetwork.h"
+#include "atSem.h"
+#include "atShm.h"
 #include "atSleep.h"
 #include "atStr.h"
 #include "atSymbols.h"
