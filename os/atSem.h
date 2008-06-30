7de0fd6ec89d1e377b235c304c9b590169182db2
jsmith <>
2008-06-30 21:25:37 +0000

    Defined a INVALID_SEM_ID.
    Wrapped the functions so they would be considered C functions.

--- os/atSem.h
+++ os/atSem.h
@@ -5,21 +5,30 @@
 
 // Under Windows, define stuff that we need
 #ifdef _MSC_VER
+   #include <windows.h>
+
    typedef HANDLE   SemID;
    typedef u_long   SemKey;
+
+   #define INVALID_SEM_ID INVALID_HANDLE_VALUE
 #else
    #include <errno.h>
    #include <sys/sem.h>
 
    typedef int     SemID;
    typedef key_t   SemKey;
+
+   #define INVALID_SEM_ID -1
 #endif
 
 
-bool    semGet(SemKey key, SemID * id);
-void    semRemove(SemID id);
-int     semLock(SemID id);
-int     semUnlock(SemID id);
+extern "C"
+{
+   bool    semGet(SemKey key, SemID * id);
+   void    semRemove(SemID id);
+   int     semLock(SemID id);
+   int     semUnlock(SemID id);
+}
 
 
 #endif
