2baf055835bb447e044702a3d6265947f87d2139
jsmith <>
2008-06-30 21:55:40 +0000

    Wrapped the function definitions so they are C functions.

--- os/atDynamic.h
+++ os/atDynamic.h
@@ -3,10 +3,13 @@
 #define AT_DYNAMIC_H
 
 
-void *   dlOpen(const char * filename, int flag);
-char *   dlError();
-void *   dlSym(void * handle, const char * symbol);
-int      dlClose(void * handle);
+extern "C"
+{
+   void *   dlOpen(const char * filename, int flag);
+   char *   dlError();
+   void *   dlSym(void * handle, const char * symbol);
+   int      dlClose(void * handle);
+}
 
 
 #endif
