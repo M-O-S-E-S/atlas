aa657cd407415df7db5b3f71eff4eb984d6e4fc1
jsmith <>
2008-06-30 21:24:47 +0000

    Added a INVALID_SHM_ID define.
    wrapped the function so they would be considered C functions.

--- os/atShm.h
+++ os/atShm.h
@@ -5,8 +5,12 @@
 
 // Under Windows, define stuff that we need
 #ifdef _MSC_VER
+   #include <windows.h>
+
    typedef HANDLE   ShmID;
    typedef u_long   ShmKey;
+
+   #define INVALID_SHM_ID INVALID_HANDLE_VALUE
 #else
    #include <errno.h>
    #include <stdlib.h>
@@ -16,13 +20,17 @@
 
    typedef int     ShmID;
    typedef key_t   ShmKey;
+
+   #define INVALID_SHM_ID -1
 #endif
 
-
-bool       shmGet(ShmKey key, u_long memSize, ShmID * id);
-void       shmRemove(ShmID id);
-u_char *   shmAttach(ShmID id);
-int        shmDetach(u_char * mem);
+extern "C"
+{
+   bool       shmGet(ShmKey key, u_long memSize, ShmID * id);
+   void       shmRemove(ShmID id);
+   u_char *   shmAttach(ShmID id);
+   int        shmDetach(u_char * mem);
+}
 
 
 #endif
