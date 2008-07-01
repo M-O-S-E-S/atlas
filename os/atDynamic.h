d588ae266af2632454fb884057beccf8303a130a
jsmith <>
2008-07-01 20:05:42 +0000

    Added defines for the dlopen flags RTLD_GLOBAL, RTLD_LAZY, and RTLD_NOW. These are windows only defines and they map to zero since the windows version of the dl functions don't need any flags.

--- os/atDynamic.h
+++ os/atDynamic.h
@@ -3,6 +3,13 @@
 #define AT_DYNAMIC_H
 
 
+#ifdef _MSC_VER
+   #define RTLD_GLOBAL   0
+   #define RTLD_LAZY   0
+   #define RTLD_NOW   0
+#endif
+
+
 extern "C"
 {
    void *   dlOpen(const char * filename, int flag);
