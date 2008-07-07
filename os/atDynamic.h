89e27638413cad15ab95df44e71162e797ffd22d
jsmith <>
2008-07-07 21:43:17 +0000

    Defines the RTLD_GLOBAL, RTLD_NOW, and RTLD_LAZY for windows. These are defined to be 0.
    Added the #include of dlfcn.h for non windows versions.

--- os/atDynamic.h
+++ os/atDynamic.h
@@ -2,20 +2,23 @@
 #ifndef AT_DYNAMIC_H
 #define AT_DYNAMIC_H
 
+#include "atSymbols.h"
 
 #ifdef _MSC_VER
    #define RTLD_GLOBAL   0
-   #define RTLD_LAZY     0
-   #define RTLD_NOW     0
+   #define RTLD_LAZY   0
+   #define RTLD_NOW   0
+#else
+   #include <dlfcn.h>
 #endif
 
 
 extern "C"
 {
-   void *   dlOpen(const char * filename, int flag);
-   char *   dlError();
-   void *   dlSym(void * handle, const char * symbol);
-   int      dlClose(void * handle);
+   ATLAS_SYM void *   dlOpen(const char * filename, int flag);
+   ATLAS_SYM char *   dlError();
+   ATLAS_SYM void *   dlSym(void * handle, const char * symbol);
+   ATLAS_SYM int      dlClose(void * handle);
 }
 
 
