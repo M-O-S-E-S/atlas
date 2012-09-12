5eb109d57b4bb425439c2a981da6c861a50cda6d
Glenn Martin <martin@ist.ucf.edu>
2012-09-12 20:57:06 +0000

    Added Android file.

---
+++ Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := atlas
+LOCAL_SRC_FILES := libatlas.so
+LOCAL_EXPORT_C_INCLUDES := \
+   $(LOCAL_PATH)/communication \
+   $(LOCAL_PATH)/container \
+   $(LOCAL_PATH)/foundation \
+   $(LOCAL_PATH)/math \
+   $(LOCAL_PATH)/os \
+   $(LOCAL_PATH)/util \
+   $(LOCAL_PATH)/xml \
+
+LOCAL_EXPORT_CFLAGS := -DATLAS_SYM=IMPORT
+LOCAL_EXPORT_CPPFLAGS := -DATLAS_SYM=IMPORT
+
+LOCAL_SHARED_LIBRARIES := glob-shared
+LOCAL_STATIC_LIBRARIES := libxml2-static
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+$(call import-module, libxml2)
+$(call import-module, glob)
+
